42bf26659b7193179a5acf428b98c3a203a332f7
Shawn Lu <42988703+chees-e@users.noreply.github.com>
2023-01-30 14:25:52 -0800

    Add files via upload

--- snake/snake.py
+++ snake/snake.py
@@ -1,62 +1,206 @@
+import time
+
+import pygame
 import pygame as pg
+import pygame.sprite
+import random as r
 
 
-class Global:
-    size = width, height = 520, 520
-
+# Global variables
+class G:
     speed = [200, 100, 50]
     speed_idx = 1
 
     spacing = 24
-    snake_width = 23
-    food_width = 20
+    snake_width = 22
+    food_width = 22
+
+    border_ratio = 0.8
+    border_thickness = 2
+
+    gridx = 20
+    gridy = 20
+
+    size = width, height = round(gridx * spacing / border_ratio), round(gridy * spacing / border_ratio)
 
     snake_colour = "white"
     food_colour = "gold"
     bg_colour = "black"
     border_colour = "white"
 
-    border_ratio = 0.8
-    border_thickness = 2
-
     tick_rate = 60
 
 
-
-
 def drawBorder(screen):
-    width_offset = (1 - Global.border_ratio) * Global.width // 2
-    height_offset = (1 - Global.border_ratio) * Global.height // 2
+    width_offset = round((1 - G.border_ratio) * G.width / 2 - G.border_thickness - (G.spacing - G.snake_width)/2)
+    height_offset = round((1 - G.border_ratio) * G.height / 2 - G.border_thickness - (G.spacing - G.snake_width)/2)
 
     corners = [
-        [width_offset, height_offset],
-        [Global.width - width_offset, height_offset],
-        [Global.width - width_offset, Global.height - height_offset],
-        [width_offset, Global.height - height_offset],
+        [width_offset-1, height_offset-1],
+        [G.width - width_offset-1, height_offset-1],
+        [G.width - width_offset-1, G.height - height_offset-1],
+        [width_offset-1, G.height - height_offset-1],
     ]
 
+    print(corners)
+
     for i in range(-1, 3):
-        pg.draw.line(screen, Global.border_colour, corners[i], corners[i+1], Global.border_thickness)
+        pg.draw.line(screen, G.border_colour, corners[i], corners[i + 1], G.border_thickness)
 
     pg.display.flip()
 
 
 class Food:
     def __init__(self, x, y, screen):
-        pg.draw.circle(screen, Global.food_colour, (x,y), Global.food_width//2)
+        self.x = x
+        self.y = y
+        self.screen = screen
+
+        self.draw()
+
+    def draw(self):
+        pg.draw.circle(self.screen, G.food_colour, (self.x, self.y), G.food_width // 2)
         pg.display.flip()
 
     def clear(self, screen):
-        screen.fill(Global.bg_colour)
+        screen.fill(G.bg_colour)
         drawBorder(screen)
 
 
-class Body:
-    pass
+class Body(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([G.snake_width, G.snake_width])
+        self.image.fill(G.snake_colour)
+        self.x = x
+        self.y = y
+        self.rect = self.image.get_rect()
+
+
+    def update(self):
+        self.rect.x = self.x - G.snake_width//2
+        self.rect.y = self.y - G.snake_width//2
+
+    def clear(self):
+        pass
+        #TODO
 
 
 class Snake:
-    pass
+    def __init__(self, x, y, screen):
+        self.x = x
+        self.y = y
+        self.screen = screen
+
+        self.alive = True
+        self.length = 1
+        self.direction = "R"
+
+        self.body = []
+        self.body.append(Body(x, y))
+        self.group = pg.sprite.Group()
+        self.group.add(self.body[0])
+
+        width_offset = round((1 - G.border_ratio) * G.width / 2 + G.spacing / 2)
+        height_offset = round((1 - G.border_ratio) * G.height / 2 + G.spacing / 2)
+
+        self.Rlimit = width_offset
+        self.Ulimit = height_offset
+        self.Llimit = self.Rlimit + (G.gridx-1) * G.spacing
+        self.Dlimit = self.Ulimit + (G.gridy-1) * G.spacing
+
+        # todo: Fix limits
+
+        rx = r.randrange(self.Rlimit, self.Llimit, G.spacing)
+        ry = r.randrange(self.Ulimit, self.Dlimit, G.spacing)
+        self.food = Food(rx, ry, screen)
+
+        self.draw()
+
+    def draw(self):
+        self.clear()
+        self.food.draw()
+        self.group.draw(self.screen)
+        pg.display.flip()
+
+    def clear(self):
+        self.food.clear(self.screen)
+        for b in self.body:
+            b.clear()
+
+    def move(self):
+        prevx = self.x
+        prevy = self.y
+
+        if self.direction == "L":
+            if self.x - G.spacing < self.Rlimit:
+                newx = self.Llimit
+            else:
+                newx = self.x - G.spacing
+            newy = self.y
+
+        if self.direction == "R":
+            print(self.x, self.Llimit)
+
+            if self.x + G.spacing > self.Llimit:
+                newx = self.Rlimit
+            else:
+                newx = self.x + G.spacing
+            newy = self.y
+
+        if self.direction == "U":
+            if self.y - G.spacing < self.Ulimit:
+                newy = self.Dlimit
+            else:
+                newy = self.y - G.spacing
+            newx = self.x
+
+        if self.direction == "D":
+            if self.y + G.spacing > self.Dlimit:
+                newy = self.Ulimit
+            else:
+                newy = self.y + G.spacing
+            newx = self.x
+
+        for i in range(self.length - 1):
+            if newx == self.body[i].x and newy == self.body[i].y:
+                self.alive = False
+                self.clear()
+                return
+
+        if newx == self.food.x and newy == self.food.y:
+            self.body.append(Body(newx, newy))
+            self.group.add(self.body[-1])
+            self.length += 1
+            rx = r.randrange(self.Rlimit, self.Llimit, G.spacing)
+            ry = r.randrange(self.Ulimit, self.Dlimit, G.spacing)
+            self.food = Food(rx, ry, self.screen)
+
+        self.body[-1].x = newx
+        self.body[-1].y = newy
+        self.body[-1].update()
+
+        print(self.body)
+
+        last = self.body.pop(-1)
+        self.body.insert(0, last)
+        self.x = newx
+        self.y = newy
+
+        self.draw()
+
+
+    def l(self):
+        pass
+
+    def r(self):
+        pass
+
+    def u(self):
+        pass
+
+    def d(self):
+        pass
 
 
 class Game:
@@ -76,30 +220,41 @@ class Game:
     def run(self):
         clock = pg.time.Clock()
 
+        # g = pg.sprite.Group()
+        # b = Body()
+        # g.add(b)
+        #
+        # g.draw(self.screen)
+        # pg.display.flip()
+
+        init_x = round((1 - G.border_ratio) * G.width / 2 + (G.gridx//2) * G.spacing  + G.spacing / 2)
+        init_y = round((1 - G.border_ratio) * G.height / 2 + (G.gridy//2) * G.spacing  + G.spacing / 2)
+        s = Snake(init_x, init_y, self.screen)
+
         while True:
             if self.check_end():
                 self.exit_game()
                 return
 
-            clock.tick(Global.tick_rate)
+            s.move()
 
+            time.sleep(1)
 
-
+            clock.tick(G.tick_rate)
 
 
 def main():
     pg.init()
 
     pg.display.set_caption("snek")
-    screen = pg.display.set_mode(Global.size)
-    screen.fill(Global.bg_colour)
+    screen = pg.display.set_mode(G.size)
+    screen.fill(G.bg_colour)
     drawBorder(screen)
-
 
     game = Game(screen)
     game.run()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
     pg.quit()
